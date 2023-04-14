1e5d01ed098c1c3b8f90315294651ac169e63e58
WY <wuyouyan1@gmail.com>
2023-04-14 20:22:31 +0200

    init express project
      add routes for contact

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const dotenv = require("dotenv").config();
+//https://www.youtube.com/watch?v=H9M02of22z4
+
+console.log("Hello World");
+
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/api/contacts', require('./routes/contact.route'));
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
